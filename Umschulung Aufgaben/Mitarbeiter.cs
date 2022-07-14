c2adf80861f62fda3f653ab594e38c60b7e000f2
geiert <geiert@PC7961>
2022-07-14 10:03:37 +0200

    Projektdateien hinzufügen.

---
+++ Umschulung Aufgaben/Mitarbeiter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umschulung_Aufgaben
+{
+    internal class Mitarbeiter
+    {
+        private string Name  { get; }
+        private decimal Gehalt { get; }
+        private Abteilung Arbeit;
+        public enum Abteilung { Buchhaltung, Produktion }
+
+
+        public Mitarbeiter(string _name, decimal _gehalt, Abteilung _abteilung)
+        {
+            Name = _name;
+            Gehalt = _gehalt;
+            Arbeit = _abteilung;
+        }   
+
+        public Mitarbeiter(string _name, Abteilung _abteilung)
+        {
+            Name = _name;
+            Arbeit = _abteilung;
+            Gehalt = 1500m;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "| Abteilung: " + Arbeit + "| Gehalt: " + Gehalt; 
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Mitarbeiter)
+            {
+            Mitarbeiter Cast = obj as Mitarbeiter;
+            return this.Name == Cast.Name && this.Arbeit == Cast.Arbeit && this.Gehalt == Cast.Gehalt;
+            }
+            return false;
+        }
+    }
+}
---
+++ Umschulung Aufgaben/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umschulung_Aufgaben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Mitarbeiter ma1 = new Mitarbeiter("Meier", 1500m, Mitarbeiter.Abteilung.Buchhaltung);
+            Mitarbeiter ma2 = new Mitarbeiter("Schulze", 1500m, Mitarbeiter.Abteilung.Produktion);
+            Mitarbeiter ma3 = new Mitarbeiter("Meier", 1500m, Mitarbeiter.Abteilung.Buchhaltung);
+
+
+            Console.WriteLine("ma1: " + ma1);
+            Console.WriteLine("ma2: " + ma2);
+            Console.WriteLine("ma3: " + ma3);
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------Vergleich von ma1 und ma2--------------------- ");
+            Console.WriteLine();
+
+            bool gleich = ma1.Equals(ma3);
+            Console.WriteLine(gleich);
+            Console.ReadLine();
+        }
+    }
+}
